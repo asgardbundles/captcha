65bcfeb4add086745ec7c17311d411d124030b30
Michel Hognerud <leyou.m@gmail.com>
2014-08-07 16:23:01 +0200

    fixes

--- Tests/CaptchaTest.php
+++ Tests/CaptchaTest.php
@@ -1,22 +1,13 @@
 <?php
 class CaptchaTest extends PHPUnit_Framework_TestCase {
-	public static function setUpBeforeClass() {
-		if(!defined('_ENV_'))
-			define('_ENV_', 'test');
-		\Asgard\Container\Container::instance(true)->config->set('bundles', array(
-				__DIR__.'/..',
-				new \Asgard\Http\Bundle
-		))
-		->set('bundlesdirs', []);
-		\Asgard\Container\Container::loadDefaultApp(false);
-	}
-	
 	public function test1() {
+		\Asgard\Container\Container::singleton()['request'] = new \Asgard\Http\Request;
 		$img = Asgard\Captcha\Libs\Captcha::image();
 		$this->assertTrue(is_resource($img));
 		$this->assertEquals('gd', get_resource_type($img));
 
-		$response = Asgard\Http\Controller::run('Asgard\Captcha\Controllers\CaptchaController', 'captcha');
+		$controller = new \Asgard\Captcha\Controllers\CaptchaController;
+		$response = $controller->run('captcha');
 		$this->assertEquals('image/jpeg', $response->getHeader('content-type'));
 		$this->assertRegExp('/CREATOR: gd-jpeg v1.0/', $response->getContent());
 	}
